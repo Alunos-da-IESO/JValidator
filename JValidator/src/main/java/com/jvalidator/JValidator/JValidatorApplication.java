d26b6dcf3404e49675dd7d6ca4cddb2309a4414f
Duduxs <duduxss3@gmail.com>
2020-10-14 20:40:59 -0300

    eduardo - Project structure altered, spring implemented

---
+++ JValidator/src/main/java/com/jvalidator/JValidator/JValidatorApplication.java
@@ -0,0 +1,13 @@
+package com.jvalidator.JValidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JValidatorApplication.class, args);
+	}
+
+}
---
+++ JValidator/src/test/java/com/jvalidator/JValidator/JValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jvalidator.JValidator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
